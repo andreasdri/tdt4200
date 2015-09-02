dd64df5deaabcad6776855bd436588bbbbdf1d6e
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-09-02 12:48:29 +0200

    Convert indentation to spaces

--- ps1/computeMPI.c
+++ ps1/computeMPI.c
@@ -4,41 +4,41 @@
 #include <math.h>
 
 /*
-	A simple MPI example.
-	TODO:
-	1. Fill in the needed MPI code to make this run on any number of nodes.
-	2. The answer must match the original serial version.
-	3. Think of corner cases (valid but tricky values).
+    A simple MPI example.
+    TODO:
+    1. Fill in the needed MPI code to make this run on any number of nodes.
+    2. The answer must match the original serial version.
+    3. Think of corner cases (valid but tricky values).
 
-	Example input:
-	./simple 2 10000
+    Example input:
+    ./simple 2 10000
 
 */
 
 int main(int argc, char **argv) {
-	int rank, size;
+    int rank, size;
 
-	if (argc < 3) {
-		printf("This program requires two parameters:\n \
+    if (argc < 3) {
+        printf("This program requires two parameters:\n \
 the start and end specifying a range of positive integers in which \
 start is 2 or greater, and end is greater than start.\n");
-		exit(1);
-	}
+        exit(1);
+    }
 
-	int start = atoi(argv[1]);
-	int stop = atoi(argv[2]);
+    int start = atoi(argv[1]);
+    int stop = atoi(argv[2]);
 
 
-	if(start < 2 || stop <= start){
-		printf("Start must be greater than 2 and the end must be larger than start.\n");
-		exit(1);
-	}
+    if(start < 2 || stop <= start){
+        printf("Start must be greater than 2 and the end must be larger than start.\n");
+        exit(1);
+    }
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	// Compute the local range, so that all the elements are accounted for.
+    // Compute the local range, so that all the elements are accounted for.
     int quotient = (stop - start) / size;
     int rest = (stop - start) % size;
     int begin = (rank * quotient) + start;
@@ -49,20 +49,22 @@ start is 2 or greater, and end is greater than start.\n");
         end += rest;
     }
 
-	// Perform the computation
-	double sum = 0.0;
-	for (int i = begin; i < end ; i++) {
-		sum += 1.0/log(i);
-	}
+    // Perform the computation
+    double sum = 0.0;
+    int i;
+    for (i = begin; i < end ; i++) {
+        sum += 1.0/log(i);
+    }
 
-	// Get the sum of each node's partial answer
+    // Get the sum of each node's partial answer
     if (rank != 0) {
         MPI_Send(&sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     else {
         double recv;
-        for (int i = 1; i < size; i++) {
+        int i;
+        for (i = 1; i < size; i++) {
             MPI_Recv(&recv, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             sum += recv;
         }
@@ -73,6 +75,6 @@ start is 2 or greater, and end is greater than start.\n");
 
     MPI_Finalize();
 
-	return 0;
+    return 0;
 }
 
