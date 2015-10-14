8d5019a937f5d9868f042a02e2f85774f021b700
andersob <andersobjornoy@gmail.com>
2015-10-14 21:16:23 +0200

    Make helpfunctions, calculate triplets with 3xfoorloop for first case in input

--- ps5/pythagoreanTriplets.c
+++ ps5/pythagoreanTriplets.c
@@ -56,10 +56,6 @@ int main(int argc, char **argv) {
 
 	int tot_threads, current_start, current_stop;
 	for (i = 0; i < amountOfRuns; ++i){
-
-		// Read in each line of input that follows after first line
-		//free(inputLine); lineLength = 0; inputLine = NULL;
-		//ssize_t readChars = getline(&inputLine, &lineLength, stdin);
 		
 		//printf("buff: %s\n", buff);
 		
@@ -94,11 +90,101 @@ int main(int argc, char **argv) {
 	*	Remember to only print 1 (one) sum per start/stop.
 	*	In other words, a total of <amountOfRuns> sums/printfs.
 	*/
+	
+	// Flinkere på testing i python...
+	// printf("isValidTrapiez returns: %i \n", isValidTrapiez(3,4,5));
+	// printf("gcd returns: %i \n", gcd(3,9));
+	// printf("elements_are_unique returns: %i \n", elements_are_unique(3,9,10));
+	// printf("is valid sum %i \n", gives_valid_triplet_sum(3,4,5));
+	// printf("no_common_dividors returns: %i \n", no_common_dividors(3,4,5));
+	// printf("3,4,5 is a valid trapiez: %i \n", isValidTrapiez(3,4,5));
+	// printf("10,24,43 is a valid trapiez: %i \n", isValidTrapiez(10,24,43));
+	// printf("45,28,53 is a valid trapiez: %i \n", isValidTrapiez(45,28,53));
+
+	int a,b,c;
+	int sum = 0;
+	for(c=start[0]; c < stop[0]; ++c){
+		for(b=4; b < c; ++b){
+			for(a=3; a < b; ++a){
+				if (isValidTrapiez(a,b,c))
+				{
+					printf("[%i,%i,%i] is valid\n", a,b,c);
+					sum++;
+				}
+			}
+		}
+	}
+
 	fclose(fp);
-	int sum;
 	printf("%d\n", sum);
 
 	return 0;
 }
 
-//make help function to se if they have gcd == 1...
+int isValidTrapiez(int a, int b, int c){
+	// things to check 
+		// [x] all are different/unique - could probably be checked elsewhere, like a cuntinue tag in loop
+		// gcd of all are 1
+		// rightsum a*a+b*b == c*c
+	if (elements_are_unique(a,b,c) == 0){
+		return 0;
+	}
+	else if (no_common_dividors(a,b,c) == 0)
+	{
+		return 0;
+	}
+	else if (gives_valid_triplet_sum(a,b,c) == 0)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+int gcd(x,y){
+	// optimalize with whileloop instad of requsive?
+	if (y==0)
+	{
+		return x;
+	}
+	else
+	{
+		return gcd(y, x % y);
+	}
+}
+
+int no_common_dividors(x,y,z){
+	// returns true if gcd between all of them is 1
+	if(gcd(x,y)==1 && gcd(x,z)==1 && gcd(y,z)==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int elements_are_unique(int x, int y, int z){
+	if (x != y && y != z && x != z)
+	{
+		return 1;	
+	}
+	else
+	{
+		return 0;	
+	}
+
+
+}
+
+int gives_valid_triplet_sum(int x, int y, int z){
+	//checks that x*x + y*y == z*z
+	if (x*x + y*y == z*z)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
