69f03a83297f07d2d2a669295944d9d4e1767d1f
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-10-17 18:50:58 +0200

    Try with OpenMPI

--- ps5/pythagoreanTriplets.c
+++ ps5/pythagoreanTriplets.c
@@ -39,31 +39,61 @@ int main(int argc, char **argv) {
 	char *inputLine = NULL; size_t lineLength = 0;
 	int *start, *stop, *numThreads, amountOfRuns = 0;
 
-	// Read in first line of input
-	getline(&inputLine, &lineLength, stdin);
-	sscanf(inputLine, "%d", &amountOfRuns);
+	#ifdef HAVE_MPI
+	int rank;
+	int size;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	#else
+	int rank = 0;
+	int size = 1;
+	#endif
+
+	if (rank == 0) {
+		// Read in first line of input
+		getline(&inputLine, &lineLength, stdin);
+		sscanf(inputLine, "%d", &amountOfRuns);
+	}
+
+	#ifdef HAVE_MPI
+	MPI_Bcast(&amountOfRuns, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	#endif
 
 	stop = (int*) calloc(amountOfRuns, sizeof(int));
 	start = (int*) calloc(amountOfRuns, sizeof(int));
 	numThreads = (int*) calloc(amountOfRuns, sizeof(int));
 
-	int tot_threads, current_start, current_stop;
-	for (int i = 0; i < amountOfRuns; ++i){
+	if (rank == 0) {
+		int tot_threads, current_start, current_stop;
+		for (int i = 0; i < amountOfRuns; ++i){
 
-		// Read in each line of input that follows after first line
-		free(inputLine); lineLength = 0; inputLine = NULL;
-		ssize_t readChars = getline(&inputLine, &lineLength, stdin);
+			// Read in each line of input that follows after first line
+			free(inputLine); lineLength = 0; inputLine = NULL;
+			ssize_t readChars = getline(&inputLine, &lineLength, stdin);
 
-		// If there exists at least two matches (2x %d)...
-		if (sscanf(inputLine, "%d %d %d", &current_start, &current_stop, &tot_threads) >= 2){
-			if(current_start < 0 || current_stop < 0){
-				current_start = 0, current_stop = 0;
+			// If there exists at least two matches (2x %d)...
+			if (sscanf(inputLine, "%d %d %d", &current_start, &current_stop, &tot_threads) >= 2){
+				if(current_start < 0 || current_stop < 0){
+					current_start = 0, current_stop = 0;
+				}
+				stop[i] = current_stop;
+				start[i] = current_start;
+				numThreads[i] = tot_threads;
+
+				if (tot_threads < 1 || tot_threads > 10) {
+					numThreads[i] = 1;
+				}
+
 			}
-			stop[i] = current_stop;
-			start[i] = current_start;
-			numThreads[i] = tot_threads;
 		}
 	}
+
+	#ifdef HAVE_MPI
+	MPI_Bcast(start, amountOfRuns, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(stop, amountOfRuns, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(numThreads, amountOfRuns, MPI_INT, 0, MPI_COMM_WORLD);
+	#endif
 
 	/*
 	*	Remember to only print 1 (one) sum per start/stop.
@@ -71,17 +101,36 @@ int main(int argc, char **argv) {
 	*/
 
 
-
 	for (int i = 0; i < amountOfRuns; i++) {
+
+
 		int current_start = start[i];
 		int current_stop = stop[i];
 		int threads = numThreads[i];
-		int sum = 0;
 
-		int b;
+		int sum = 0;
+		int totalSum;
+
+		#ifdef HAVE_MPI
+		// Compute the local range, so that all the elements are accounted for.
+	    int quotient = (current_stop - 4) / size;
+	    int rest = (current_stop - 4) % size;
+	    int begin = (rank * quotient) + 4;
+		int end = (rank * quotient) + quotient + 4;
+		// The last process must do some more work
+	    if (rank + 1 == size) {
+	        end += rest;
+	    }
+
+ 		#else
+ 		int begin = 4;
+ 		int end = current_stop;
+ 		#endif
+
+ 		int b;
 		# pragma omp parallel for num_threads(threads) \
 			reduction(+: sum) schedule(static, 1)
-		for (b = 4; b < current_stop; b++) {
+		for (b = begin; b < end; b++) {
 			int a;
 			for (a = 3; a < b; a++) {
 				int c = (int) sqrt(a * a + b * b);
@@ -89,13 +138,22 @@ int main(int argc, char **argv) {
 					sum++;
 				}
 			}
+
 		}
 
+		#ifdef HAVE_MPI
+		MPI_Reduce(&sum, &totalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		if(rank == 0)
+			printf("%d\n", totalSum);
+		#else
 		printf("%d\n", sum);
+		#endif
+
 	}
 
-
-
+	#ifdef HAVE_MPI
+	MPI_Finalize();
+	#endif
 
 	return 0;
 }
