0099b7d5c5419782955e8a8aa7c8dd98b8d4aac8
andersob <andersobjornoy@gmail.com>
2015-10-28 08:12:59 +0100

    change to vector most places, not working propperly

--- ps6/newImageIdea.c
+++ ps6/newImageIdea.c
@@ -10,24 +10,27 @@
 // http://7-themes.com/6971875-funny-flowers-pictures.html
 
 typedef struct {
-    float red,green,blue; // TODO: this must probably be changed for a SIMD version.
-} AccuratePixel;
+    float red,green,blue,trash; // TODO: this must probably be changed for a SIMD version.
+} AccuratePixelSIMD;
 
 typedef struct {
      int x, y;
-     AccuratePixel *data; // TODO: this might require some changes for a SIMD version too.
+     AccuratePixelSIMD *data; // TODO: this might require some changes for a SIMD version too.
 } AccurateImage;
+
+typedef float v4f __attribute__((vector_size (16)));
 
 // Convert ppm to high precision format.
 AccurateImage *convertImageToNewFormat(PPMImage *image) {
 	// Make a copy
 	AccurateImage *imageAccurate;
 	imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
+	imageAccurate->data = (AccuratePixelSIMD*)malloc(image->x * image->y * sizeof(AccuratePixelSIMD));
 	for(int i = 0; i < image->x * image->y; i++) {
 		imageAccurate->data[i].red   = (float) image->data[i].red;
 		imageAccurate->data[i].green = (float) image->data[i].green;
 		imageAccurate->data[i].blue  = (float) image->data[i].blue;
+		//imageAccurate->data[i].trash = (float) 0; // prove aa fjerne denne senere
 	}
 	imageAccurate->x = image->x;
 	imageAccurate->y = image->y;
@@ -38,7 +41,7 @@ AccurateImage *convertImageToNewFormat(PPMImage *image) {
 AccurateImage *createEmptyImage(PPMImage *image){
 	AccurateImage *imageAccurate;
 	imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
+	imageAccurate->data = (AccuratePixelSIMD*)malloc(image->x * image->y * sizeof(AccuratePixelSIMD));
 	imageAccurate->x = image->x;
 	imageAccurate->y = image->y;
 	
@@ -55,14 +58,24 @@ void freeImage(AccurateImage *image){
 void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn,int size) {
 	int countIncluded = 0;
 	int offsetOfThePixel=0;
-	float sum_red = 0;
-	float sum_blue = 0;
-	float sum_green =0;
+	//float sum_red = 0;
+	//float sum_blue = 0;
+	//float sum_green =0;
+	v4f sum;
 	int numberOfValuesInEachRow = imageIn->x;
 
+	v4f *imageArray = (v4f*) imageIn->data;
+	//printf("%i\n", imageArray[0][1]);
+	//v4f *line_buffer;
+	//posix_memalign(&line_buffer, 32, imageIn->x*sizeof(v4f));
+	
 	// line buffer that will save the sum of some pixel in the column
-	AccuratePixel *line_buffer = (AccuratePixel*) malloc(imageIn->x*sizeof(AccuratePixel));
-	memset(line_buffer,0,imageIn->x*sizeof(AccuratePixel));
+	v4f *line_buffer = (AccuratePixelSIMD*) malloc(imageIn->x*sizeof(AccuratePixelSIMD));
+	//AccuratePixelSIMD *line_buffer = (AccuratePixelSIMD*) malloc(imageIn->x*sizeof(AccuratePixelSIMD));
+	memset(line_buffer,0,imageIn->x*sizeof(AccuratePixelSIMD));
+	
+	//v4f *line_buffer; 
+	//posix_memalign(&line_buffer, 32, imageIn->x*sizeof(16)); // v4f er 16 tror jeg
 
 	// Iterate over each line of pixelx.
 	for(int senterY = 0; senterY < imageIn->y; senterY++) {
@@ -77,17 +90,19 @@ void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn,int
 				// we save the result in the array line_buffer
 				for(int line_y=starty; line_y < endy; line_y++){
 					for(int i=0; i<imageIn->x; i++){
-						line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*line_y+i].blue;
-						line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*line_y+i].red;
-						line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*line_y+i].green;
+						line_buffer[i] += imageArray[numberOfValuesInEachRow*line_y+i];
+						//line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*line_y+i].blue;
+						//line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*line_y+i].red;
+						//line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*line_y+i].green;
 					}
 				}
 			}
 			for(int i=0; i<imageIn->x; i++){
 				// add the next pixel of the next line in the column x
-				line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*endy+i].blue;
-				line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*endy+i].red;
-				line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*endy+i].green;
+				line_buffer[i] += imageArray[numberOfValuesInEachRow*endy+i];
+				//line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*endy+i].blue;
+				//line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*endy+i].red;
+				//line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*endy+i].green;
 			}
 
 		}
@@ -96,23 +111,26 @@ void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn,int
 			// for the last lines, we just need to subtract the first added line
 			endy = imageIn->y-1;
 			for(int i=0; i<imageIn->x; i++){
-				line_buffer[i].blue-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].blue;
-				line_buffer[i].red-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].red;
-				line_buffer[i].green-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].green;
+				line_buffer[i] -= imageArray[numberOfValuesInEachRow*(starty-1)+1];
+				//line_buffer[i].blue-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].blue;
+				//line_buffer[i].red-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].red;
+				//line_buffer[i].green-=imageIn->data[numberOfValuesInEachRow*(starty-1)+i].green;
 			}	
 		}else{
 			// general case
 			// add the next line and remove the first added line
 			for(int i=0; i<imageIn->x; i++){
-				line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*endy+i].blue-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].blue;
-				line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*endy+i].red-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].red;
-				line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*endy+i].green-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].green;
+				line_buffer[i] += imageArray[numberOfValuesInEachRow*endy+i] - imageArray[numberOfValuesInEachRow*(starty-1)+i];
+				//line_buffer[i].blue+=imageIn->data[numberOfValuesInEachRow*endy+i].blue-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].blue;
+				//line_buffer[i].red+=imageIn->data[numberOfValuesInEachRow*endy+i].red-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].red;
+				//line_buffer[i].green+=imageIn->data[numberOfValuesInEachRow*endy+i].green-imageIn->data[numberOfValuesInEachRow*(starty-1)+i].green;
 			}	
 		}
-		
-		sum_green =0;
-		sum_red = 0;
-		sum_blue = 0;
+
+		//sum_green =0;
+		//sum_red = 0;
+		//sum_blue = 0;
+		sum *= 0;
 		for(int senterX = 0; senterX < imageIn->x; senterX++) {
 			// in this loop, we do exactly the same thing as before but only with the array line_buffer
 
@@ -123,33 +141,40 @@ void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn,int
 				startx = 0;
 				if(senterX==0){
 					for (int x=startx; x < endx; x++){
-						sum_red += line_buffer[x].red;
-						sum_green += line_buffer[x].green;
-						sum_blue += line_buffer[x].blue;
+						sum += line_buffer[x];
+						//sum_red += line_buffer[x].red;
+						//sum_green += line_buffer[x].green;
+						//sum_blue += line_buffer[x].blue;
 					}
 				}
-				sum_red +=line_buffer[endx].red;
-				sum_green +=line_buffer[endx].green;
-				sum_blue +=line_buffer[endx].blue;
+				sum += line_buffer[endx];
+				//sum_red +=line_buffer[endx].red;
+				//sum_green +=line_buffer[endx].green;
+				//sum_blue +=line_buffer[endx].blue;
 			}else if (endx >= imageIn->x){
 				endx = imageIn->x-1;
-				sum_red -=line_buffer[startx-1].red;
-				sum_green -=line_buffer[startx-1].green;
-				sum_blue -=line_buffer[startx-1].blue;
+				sum -= line_buffer[startx-1];
+				//sum_red -=line_buffer[startx-1].red;
+				//sum_green -=line_buffer[startx-1].green;
+				//sum_blue -=line_buffer[startx-1].blue;
 
 			}else{
-				sum_red += (line_buffer[endx].red-line_buffer[startx-1].red);
-				sum_green += (line_buffer[endx].green-line_buffer[startx-1].green);
-				sum_blue += (line_buffer[endx].blue-line_buffer[startx-1].blue);
+				sum += line_buffer[endx] - line_buffer[startx-1];
+				//sum_red += (line_buffer[endx].red-line_buffer[startx-1].red);
+				//sum_green += (line_buffer[endx].green-line_buffer[startx-1].green);
+				//sum_blue += (line_buffer[endx].blue-line_buffer[startx-1].blue);
 			}			
 
 			// we save each pixel in the output image
 			offsetOfThePixel = (numberOfValuesInEachRow * senterY + senterX);
 			countIncluded=(endx-startx+1)*(endy-starty+1);
 
-			imageOut->data[offsetOfThePixel].red = sum_red/countIncluded;
-			imageOut->data[offsetOfThePixel].green = sum_green/countIncluded;
-			imageOut->data[offsetOfThePixel].blue = sum_blue/countIncluded;
+			imageOut->data[offsetOfThePixel].red = sum[0]/countIncluded;
+			imageOut->data[offsetOfThePixel].green = sum[1]/countIncluded;
+			imageOut->data[offsetOfThePixel].blue = sum[2]/countIncluded;
+			//imageOut->data[offsetOfThePixel].red = sum_red/countIncluded;
+			//imageOut->data[offsetOfThePixel].green = sum_green/countIncluded;
+			//imageOut->data[offsetOfThePixel].blue = sum_blue/countIncluded;
 		}
 	
 	}
@@ -217,7 +242,7 @@ void performNewIdeaFinalization(AccurateImage *imageInSmall, AccurateImage *imag
 
 
 int main(int argc, char** argv) {
-	
+
 	PPMImage *image;
         
 	if(argc > 1) {
